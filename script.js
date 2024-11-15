26490b6d81f5ad6e73d2c78b98076aad2325341b
umeshchandra15 <umeshchandrekanti1234@gmail.com>
2024-11-15 18:17:20 +0530

    new

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+let totalEl = document.getElementById("total-el");
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
-let count = 0;
-console.log(saveEl)
+let count = 0; 
+console.log(saveEl);
+console.log(totalEl);
 function increment(){
    count += 1;
    countEl.innerText = count
 }
  
 function save(){
-    let countStr = count + " - "
-saveEl.textContent += countStr
+    let  countStr = count + " - "; 
+saveEl.textContent += countStr;
+
+let  totalpeople =saveEl + countEl;
+totalEl.textContent += totalpeople;
+
 countEl.textcountent = 0;
 count = 0;
 
 }
+// function total(){
+//     let   totalpeople = saveEl + countEl;
+//     totalEl.textContent = totalpeople;
+
+// }
