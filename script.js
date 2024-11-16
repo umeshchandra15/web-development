19aa128eced16c0936bd0d2e78ece943d02fdda6
umeshchandra15 <umeshchandrekanti1234@gmail.com>
2024-11-16 22:42:52 +0530

    new

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-let totalEl = document.getElementById("total-el");
+
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
-let count = 0; 
+let count = 0;
 console.log(saveEl);
-console.log(totalEl);
 function increment(){
    count += 1;
    countEl.innerText = count
 }
- 
-function save(){
-    let  countStr = count + " - "; 
-saveEl.textContent += countStr;
 
-let  totalpeople =saveEl + countEl;
-totalEl.textContent += totalpeople;
-
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
 countEl.textcountent = 0;
-count = 0;
+    count = 0;
 
 }
-// function total(){
-//     let   totalpeople = saveEl + countEl;
-//     totalEl.textContent = totalpeople;
 
-// }
+let age =21;
+if( age => 21){
+    console.log("you can't enter rhe club");
+}
+else{
+    console.log("welcome!");
+}
